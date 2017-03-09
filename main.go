586cca2ac8039f0fcc2fad8efe857af37403ebc1
faparraf <faparraf@gmail.com>
2017-03-09 10:55:06 -0500

    Agregación de publicación de recursos swagger

--- main.go
+++ main.go
@@ -27,6 +27,9 @@ func main() {
 	ExposeHeaders: []string{"Content-Length"},
 	AllowCredentials: true,
 	}))
-
+	if beego.BConfig.RunMode == "dev" {
+			beego.BConfig.WebConfig.DirectoryIndex = true
+			beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+		}
 	beego.Run()
 }
