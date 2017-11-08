b6063d4bd455aa7aa05abad635d72fe753d6237a
Maria Alejandra Ortiz <mariaalejandra9404@gmail.com>
2017-11-08 15:05:35 -0500

    swagger

--- main.go
+++ main.go
@@ -12,24 +12,25 @@ import (
 func init() {
 	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGurls")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
 }
+
 func main() {
 	orm.Debug = true
-	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-	AllowOrigins: []string{"*"},
-	AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
-	AllowHeaders: []string{"Origin", "x-requested-with",
-	"content-type",
-	"accept",
-	"origin",
-	"authorization",
-	"x-csrftoken"},
-	ExposeHeaders: []string{"Content-Length"},
-	AllowCredentials: true,
-	}))
 	if beego.BConfig.RunMode == "dev" {
-			beego.BConfig.WebConfig.DirectoryIndex = true
-			beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-		}
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	beego.Run()
-	//NUEVO COMMIT
 }
