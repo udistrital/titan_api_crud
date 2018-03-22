83639f719c3c2d1cc5649bfcdd0eaa57480cf2f5
Maria Alejandra Ortiz <mariaalejandra9404@gmail.com>
2018-03-22 14:11:07 -0500

    se añade apistatus

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	_ "github.com/udistrital/titan_api_crud/routers"
-
+	"github.com/udistrital/utils_oas/apiStatusLib"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/lib/pq"
@@ -35,5 +35,6 @@ func init() {
 
 func main() {
 	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGurls")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
+	apistatus.Init()
 	beego.Run()
 }
