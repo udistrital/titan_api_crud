35f000386252c873f6c3e3b8d3bef86960488de2
Juan Camilo Sarmiento Reyes <yhomii.cloud@gmail.com>
2018-09-27 16:48:52 -0500

    Ajustada transacción para registro de conceptos
    Se cambio el método InserMulti, en su lugar se recorre el arreglo de conceptos, para insertarlos uno a uno y después devolver un arreglo con todos los id registrados

--- models/tr_concepto_nomina_por_persona.go
+++ models/tr_concepto_nomina_por_persona.go
@@ -1,23 +1,35 @@
 package models
 
-import "github.com/astaxie/beego/orm"
+import (
+	"fmt"
+
+	"github.com/astaxie/beego/orm"
+)
 
 // TrConceptosNomPersona Estructura para el registro de la transacción, es un arreglo de apuntadores de tipo ConceptoNominaPorPersona
 type TrConceptosNomPersona struct {
-	Conceptos []*ConceptoNominaPorPersona
+	Conceptos []ConceptoNominaPorPersona
 }
 
 // RegistrarConceptos transacción para registro de varios conceptos en la tabla concepto_nomina_por_persona
 func RegistrarConceptos(m *TrConceptosNomPersona) (alerta Alert, err error) {
+	var registros []int64
+	fmt.Println("concepto 0: ", m.Conceptos[0])
 	o := orm.NewOrm()
 	o.Begin()
-	_, err = o.InsertMulti(len(m.Conceptos), m.Conceptos)
-	if err == nil {
-		alerta = Alert{Type: "success", Code: "Ok", Body: err}
-		o.Commit()
-	} else {
-		alerta = Alert{Type: "error", Code: "E_CONCEPTOS", Body: err}
-		o.Rollback()
+
+	for i := 0; i < len(m.Conceptos); i++ {
+		id, err := o.Insert(&m.Conceptos[i])
+		if err != nil {
+			alerta = Alert{Type: "error", Code: "E_CONCEPTOS", Body: err}
+			o.Rollback()
+		} else {
+			registros = append(registros, id)
+		}
 	}
-	return alerta, err
+
+	alerta = Alert{Type: "success", Code: "Ok", Body: registros}
+	o.Commit()
+
+	return
 }
