3da12a938b30f9c94eac37aa070b8cc376feaac0
Maria Alejandra Ortiz <mariaalejandra9404@gmail.com>
2019-07-08 12:00:43 -0500

    Prueba de auditoría

--- main.go
+++ main.go
@@ -39,7 +39,8 @@ func init() {
 func main() {
 	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGurls")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
 	apistatus.Init()
-  auditoria.InitMiddleware()
+  //Prueba de auditoria
+	auditoria.InitMiddleware()
 	beego.ErrorController(&customerror.CustomErrorController{})
 	beego.Run()
 }
