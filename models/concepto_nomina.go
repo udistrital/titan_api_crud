99e0c04d7f52ccf1623507d5badd25da40395f7b
jorgec815 <jorgecortes815@gmail.com>
2021-12-01 13:46:39 -0500

    fix: ajuste del metodo de actualizacion

--- models/concepto_nomina.go
+++ models/concepto_nomina.go
@@ -134,7 +134,6 @@ func UpdateConceptoNominaById(m *ConceptoNomina) (err error) {
 	// ascertain id exists in the database
 	if err = o.Read(&v); err == nil {
 		var num int64
-		m.FechaCreacion = v.FechaCreacion
 		if num, err = o.Update(m); err == nil {
 			fmt.Println("Number of records updated in database:", num)
 		}
